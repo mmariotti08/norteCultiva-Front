90bf6648d1573cb5d43f8201399deabfb9395f3e
JuanCruz <juancruzgenovese22@gmail.com>
2023-08-12 16:14:41 -0300

    Tu novia puta con mi pingo se ahoga

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,6 +1,11 @@
+import style from "./Home.module.css";
+
 const Home =()=>{
-    <div>
-        <h1>soy el  home</h1>
-    </div>
+    return (
+        <div className={style.container}>
+            <h1>Tu novia puta con mi píngo se ahoga</h1>
+        </div>
+    )
+        
 }
 export default Home;
