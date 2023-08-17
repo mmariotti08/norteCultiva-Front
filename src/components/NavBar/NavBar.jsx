59f3eedcf17a417385bdea8974d2e494b2ed533c
JuanCruz <juancruzgenovese22@gmail.com>
2023-08-17 17:08:57 -0300

    Estilos nuevos en navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom";
 import style from "./NavBar.module.css";
 
-const NavBar =()=>{
+const NavBar = () => {
     return(
-        <div className={style.navBar}>
-            <div>
-                <Link to='/home'>
-                    <span className={style.namePage}>EL NORTE CULTIVA</span>
+        <div className={style.NavBar}>
+            <div className={style.Container}>
+                <Link to='/home' className={style.NavLink}>
+                    <span className={style.Title}>EL NORTE CULTIVA</span>
                 </Link>
+                <div className={style.LinksChicosContainer}>
+                    <Link to="/home" className={style.NavLink}>
+                        <span className={style.LinkChico}> Diccionario </span>
+                    </Link>
+                    <Link to="/home" className={style.NavLink}>
+                        <span className={style.LinkChico}> Sobre nosotros </span>
+                    </Link>
+                    <Link to="/home" className={style.NavLink}>
+                        <span className={style.LinkChico}> Contacto </span>
+                    </Link>
+                </div>
             </div>
 
         </div>
