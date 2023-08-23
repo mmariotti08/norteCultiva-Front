ddd579f271dafc5301ebaff2a6eb4a624ff2422b
JuanCruz <juancruzgenovese22@gmail.com>
2023-08-23 16:17:51 -0300

    Landing tienda modularizada

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./Products.module.css"
+
+const Products = () => {
+    return (
+        <div className={style.Productos}>
+            <h1> Productos </h1>
+        </div>
+    )
+}
+
+export default Products;
