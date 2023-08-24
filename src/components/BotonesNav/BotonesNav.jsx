f2d56104e8917fd5b1f710fc6c5db9b65577811d
JuanCruz <juancruzgenovese22@gmail.com>
2023-08-23 21:59:54 -0300

    Estilos en la tienda

--- src/components/BotonesNav/BotonesNav.jsx
+++ src/components/BotonesNav/BotonesNav.jsx
@@ -6,16 +6,16 @@ const BotonesNav = () => {
     return (
         <div className={style.BotonesNav}>
             <Link to="">
-                <button className={style.Boton}> PARAFERNALIA </button>
+                <button className={style.Boton}> Parafernalia </button>
             </Link>
             <Link to="">
-                <button className={style.Boton}> JARDINERIA </button>
+                <button className={style.Boton}> Jardineria </button>
             </Link>
             <Link to="">
-                <button className={style.Boton}> SEMILLAS </button>
+                <button className={style.Boton}> Semillas </button>
             </Link>
             <Link to="">
-                <button className={style.Boton}> MATERIA PRIMA </button> 
+                <button className={style.Boton}> Materia prima </button> 
             </Link>
         </div>
     )
