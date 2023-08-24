2efc19ee5ad1ab8828420473296b819378c660a6
JuanCruz <juancruzgenovese22@gmail.com>
2023-08-24 19:11:48 -0300

    consola limpia

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
         <div className={style.container}>
             <div className={`${style.botonFlotante} ${botonVisible ? style.botonVisible : style.botonInvisible}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={`bi bi-arrow-down-short ${style.botonFlotante} ${botonVisible ? style.botonVisible : style.botonInvisible}`} viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"/>
+                    <path fillRule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"/>
                 </svg>
             </div>
             <img src="./public/IMG_3731.JPG" className={style.background}></img>
