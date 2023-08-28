5ebb0d387e2d109dd647758c578896cd7eca0a4c
mmariotti08 <mariottimatias08@gmail.com>
2023-08-28 14:09:52 -0300

    solucionLogin

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import { Route, Routes } from 'react-router-dom'
 import { Home, LandingTienda, PreguntasFrecuentes, ChatGPT, SelectArea, Contact, AboutUs, Jardineria, Parafernalia, MateriaPrima, Semillas} from "./views/indexViews"
 import { Nav, LeftNav, RightNav, Footer ,LogIn} from './components/indexComponents';
 import './App.css'
+import { useState } from 'react';
 
 
 
 function App() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
   return (
    <div className="background-shadow">
       <img src="/IMG_3731.JPG" className="background-image"></img>
       <LeftNav/>
-      <Nav/>
+      <Nav onLinkClick={handleModalOpen}/>
       <RightNav/>
       <Routes>
         <Route path='/' element={<Home/>}/>
