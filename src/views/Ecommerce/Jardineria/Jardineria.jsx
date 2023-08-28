788543f4f007c8271f358da801fb2a490bef1034
JuanCruz <juancruzgenovese22@gmail.com>
2023-08-28 19:58:20 -0300

    .

--- src/views/Ecommerce/Jardineria/Jardineria.jsx
+++ src/views/Ecommerce/Jardineria/Jardineria.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import style from "./Jardineria.module.css";
 
 const Jardineria = () => {
+
+  useEffect(() => {
+    window.scroll(0,0);
+  }, []);
+  
   return (
     <div className={style.Jardineria}>
+    <form className={style.form}>
+        <input type="text" placeholder='Buscar' />
+        <button> Lupa </button>
+    </form>
+    <div className={style.products}>
       <h1>Jardineria</h1>
+    </div>
     </div>
   )
 }
