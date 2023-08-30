434b178ed519bc0ae787f33d65ba79401d1c863b
JuanCruz <juancruzgenovese22@gmail.com>
2023-08-30 19:57:25 -0300

    ,

--- src/components/FeaturedProducts/FeaturedJardineria/FeaturedJardineria.jsx
+++ src/components/FeaturedProducts/FeaturedJardineria/FeaturedJardineria.jsx
@@ -4,15 +4,14 @@ import { useSelector } from 'react-redux';
 
 const FeaturedJardineria = () => {
   const products = useSelector(state => state.products)
-  console.log(products);
   return (
     <div className={style.jardineria}>
-      <div className={style.producto1}> Producto 1 </div>
-      <div className={style.producto}> Producto 2 </div>
-      <div className={style.producto}> Producto 3 </div>
-      <div className={style.producto}> Producto 4 </div>
-      <div className={style.producto}> Producto 5 </div>
-      <div className={style.producto6}> Producto 6 </div>
+      <div className={style.producto1}>{products[0].name}</div>
+      <div className={style.producto}>{products[1].name}</div>
+      <div className={style.producto}>{products[2].name}</div>
+      <div className={style.producto}>{products[3].name}</div>
+      <div className={style.producto}>{products[4].name}</div>
+      <div className={style.producto6}>{products[5].name}</div>
     </div>    
   )
 }
